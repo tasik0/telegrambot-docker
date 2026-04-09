6faddcb701d6782aba20e5d6cec3df14ab6d0a54
tasik0 <130841446+tasik0@users.noreply.github.com>
2026-04-09 12:34:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import TelegramBot from 'node-telegram-bot-api';
 const bot = new TelegramBot('8184529006:AAEDF6ix07r-D82qU3OZITaPu4tlaXj-Xq0', { polling: true });
 
 bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, 'Bot working 🚀');
+  bot.sendMessage(msg.chat.id, 'Bot working v2 🚀');
 });
